467d3745ce980217f6f4309bf9d359e8609e17da
Arun K R <114285027+qwerty-arun@users.noreply.github.com>
2025-07-22 21:55:53 +0530

    Update solution.cpp

--- BinarySearch/singleElementSortedArray/solution.cpp
+++ BinarySearch/singleElementSortedArray/solution.cpp
@@ -3,36 +3,24 @@
 # include <iostream>
 #include <vector>
 
-int singleElement(std::vector<int> vec, int size)
+int singleNonDuplicate(vector<int>& nums) {
+int size = nums.size();
+int st{0}, end = size-1, mid{0};
+while(st<end)
 {
-	int st{0}, end = size-1,mid{0};
-	while(st<=end)
-	{
-		mid = st + (end-st)/2;
-		if(mid ==0 && vec[0]!=vec[1]) //corner case to handle vec[-1] != vec[0]
-			return vec[mid];
-		if(mid==size-1 && vec[size-2]!=vec[size-1])
-			return vec[mid];
+	mid = st + (end - st)/2;
 
-		if(vec[mid-1]!=vec[mid] and vec[mid]!=vec[mid+1])
-			return vec[mid];
-		if(mid%2==0) //even
-		{
-			if(vec[mid-1]==vec[mid])
-				end = mid-1;
-			else
-				st=mid+1;
-		}
-		else //odd
-		{
-			if(vec[mid-1]==vec[mid])
-				st=mid+1;
-			else
-				end = mid-1;
-		}
-	}
-	return -1; //fail
+        if(mid % 2==1)
+        	mid--;
+                
+        if(nums[mid] == nums[mid+1])
+                st = mid + 2;
+        else
+                end = mid;
 }
+        return nums[st];
+}
+
 int main()
 {
 	std::vector<int> vec = {1,1,2,3,3,4,4,8,8};
